b52982cbdfed369faf2599ad3a55d55d22a6bed0
MaziBai-com <101470397+MaziBai-com@users.noreply.github.com>
2022-03-22 07:04:00 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,13 @@ const dotenv = require('dotenv');
 dotenv.config()
 module.exports = {
     PORT: process.env.PORT || 5000,
-    MONGO_URL: process.env.MONGO_URL || "mongodb+srv://thisismyusername:thisismypassword@cluster0.yukep.mongodb.net/mazibai?retryWrites=true&w=majority",
+    MONGO_URL: process.env.MONGO_URL,
     MONGO_CLUSTER_URL: process.env.MONGO_CLUSTER_URL,
     JWT_SECRET: process.env.JWT_SECRET || "MYSECRET@CODE$",
     MONGOOSE_OPTIONS: {
         useNewUrlParser:true 
     },
     NODEMAILER_EMAIL:process.env.NODEMAILER_EMAIL || "mazibaiteam@gmail.com",
-    NODEMAILER_PASSWORD:process.env.NODEMAILER_PASSWORD || "Rgukt@123",
+    NODEMAILER_PASSWORD:process.env.NODEMAILER_PASSWORD,
     GOOGLE_CLIENT_ID:"746130167111-n7o5qnpn9gns3m0nf75dk72tfton7239.apps.googleusercontent.com"
 }
-
-// booking 
