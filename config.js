441d98f55bb617b87fad1886361394a6ebdc6bd0
mazibai <mazibaiteam@gmail.com>
2022-03-25 18:14:33 -0700

    backend updated

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const dotenv = require('dotenv'); 
 dotenv.config()
 module.exports = {
-    PORT: process.env.PORT || 5000,
-    MONGO_URL: process.env.MONGO_URL || "mongodb+srv://thisismyusername:thisismypassword@cluster0.yukep.mongodb.net/mazibai?retryWrites=true&w=majority",
+    PORT: process.env.PORT ,
+    MONGO_URL: process.env.MONGO_URL ,
     MONGO_CLUSTER_URL: process.env.MONGO_CLUSTER_URL,
-    JWT_SECRET: process.env.JWT_SECRET || "MYSECRET@CODE$",
+    JWT_SECRET: process.env.JWT_SECRET,
     MONGOOSE_OPTIONS: {
         useNewUrlParser:true 
     },
-    NODEMAILER_EMAIL:process.env.NODEMAILER_EMAIL || "mazibaiteam@gmail.com",
-    NODEMAILER_PASSWORD:process.env.NODEMAILER_PASSWORD || "Rgukt@123",
-    GOOGLE_CLIENT_ID:"746130167111-n7o5qnpn9gns3m0nf75dk72tfton7239.apps.googleusercontent.com"
+    NODEMAILER_EMAIL:process.env.NODEMAILER_EMAIL ,
+    NODEMAILER_PASSWORD:process.env.NODEMAILER_PASSWORD ,
+    GOOGLE_CLIENT_ID: "746130167111-n7o5qnpn9gns3m0nf75dk72tfton7239.apps.googleusercontent.com"
 }
