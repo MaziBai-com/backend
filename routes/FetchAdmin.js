5b24e8fac0dd7ccac47dcfe27c576b5648571622
mazibai <mazibaiteam@gmail.com>
2022-04-08 19:13:00 -0700

    backend

--- routes/FetchAdmin.js
+++ routes/FetchAdmin.js
@@ -1,8 +1,12 @@
-const FetchAdmin = (req,res,next)=>{
+
+const Admin = require('../models/Admin');
+
+const FetchAdmin = async (req,res,next)=>{
     // GET USER FROM JWT TOKEN 
     try {
         const email = req.header('Admin');
-        if(email === 'admin@mazibai.com'){
+        let admin = await Admin.find(); 
+        if(email === admin[0].email){  
             next()
         }else{
             res.status(400).json({success:false,msg:"you are not an admin"})
